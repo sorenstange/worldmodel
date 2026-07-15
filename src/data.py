ef509edace57338848444b7efa5facf7e1dd6820
Søren Stange <s204229@student.dtu.dk>
2026-07-15 15:18:14 +0200

    Updates to data.py

--- src/data.py
+++ src/data.py
@@ -49,10 +49,10 @@ def load_data(cfg, mode = 'train'):
 
     data, targets = [], []
 
-    df = pd.read_csv(f'./src/autotrader/LeWM/data/processed/{tf}/{symbol}.csv')
+    df = pd.read_csv(f'./data/processed/{tf}/{symbol}.csv')
     df.set_index('OpenTime', inplace = True)
   
-    df = df[((df.index >= cfg[f'{mode}_period'][0]) & (df.index < cfg[f'{mode}_period'][1]))]
+    df = df[((df.index >= cfg[f'{mode}_interval'][0]) & (df.index < cfg[f'{mode}_interval'][1]))]
 
     data.append(df.drop('Return', axis = 1).values.astype(np.float32))
     targets.append(df['Return'].shift(-1).fillna(0).values.astype(np.float32))
@@ -64,33 +64,34 @@ def update_data(cfg):
     logger.info('Updating data...')
     cfg = cfg['data']
     tf  = cfg['timeframe']
-    symbol = cfg['symbol']
+    symbols = cfg['symbol']
 
-    start_date  = min([min(cfg[period]) for period in ['train_period', 'val_period', 'test_period']])
-    end_date    = max([max(cfg[period]) for period in ['train_period', 'val_period', 'test_period']])
-    os.makedirs(f'./src/autotrader/LeWM/data/raw/{tf}', exist_ok=True)
-    
-    try:
-        path = f'./src/autotrader/LeWM/data/raw/{tf}/{symbol}.csv'
-        if not os.path.exists(path): # We need to download the data
-            logger.info(f'Data for {symbol} {tf} does not exists... Downloading data...')
-            df = get_OHLCV(symbol, tf, SINCE = start_date, TO = end_date)
-            df.to_csv(path, index = False) 
-            logger.info(f'Data for {symbol} {tf} downloaded to {path}.')
-        else:
-            df = pd.read_csv(path)
-            current_end_date = max(df['OpenTime'])
-            df['OpenTime'] = pd.to_datetime(df['OpenTime'])
+    start_date  = min([min(cfg[period]) for period in ['train_interval', 'val_interval', 'test_interval']])
+    end_date    = max([max(cfg[period]) for period in ['train_interval', 'val_interval', 'test_interval']])
+    os.makedirs(f'./data/raw/{tf}', exist_ok=True)
 
-            logger.info(f'Updating data for {symbol} {tf}...')
-            df_updated = get_OHLCV(symbol, tf, SINCE = current_end_date, TO = end_date)
-            df_updated['OpenTime'] = pd.to_datetime(df_updated['OpenTime'])
-            df = pd.concat([df, df_updated])
-            df.drop_duplicates(subset=['OpenTime'], keep = 'last', inplace = True)
-            df.to_csv(path, index = False)
-            logger.info(f'Data for {symbol} {tf} downloaded to {path}!')
-    except Exception as e:
-        logger.error(f'Error at {symbol}: {e}')
+    for symbol in symbols:
+        try:
+            path = f'./src/autotrader/LeWM/data/raw/{tf}/{symbol}.csv'
+            if not os.path.exists(path): # We need to download the data
+                logger.info(f'Data for {symbol} {tf} does not exists... Downloading data...')
+                df = get_OHLCV(symbol, tf, SINCE = start_date, TO = end_date)
+                df.to_csv(path, index = False) 
+                logger.info(f'Data for {symbol} {tf} downloaded to {path}.')
+            else:
+                df = pd.read_csv(path)
+                current_end_date = max(df['OpenTime'])
+                df['OpenTime'] = pd.to_datetime(df['OpenTime'])
+
+                logger.info(f'Updating data for {symbol} {tf}...')
+                df_updated = get_OHLCV(symbol, tf, SINCE = current_end_date, TO = end_date)
+                df_updated['OpenTime'] = pd.to_datetime(df_updated['OpenTime'])
+                df = pd.concat([df, df_updated])
+                df.drop_duplicates(subset=['OpenTime'], keep = 'last', inplace = True)
+                df.to_csv(path, index = False)
+                logger.info(f'Data for {symbol} {tf} downloaded to {path}!')
+        except Exception as e:
+            logger.error(f'Error at {symbol}: {e}')
 
     logger.info('Update complete!')
     
@@ -100,35 +101,36 @@ def preprocess_data(cfg):
     cfg = cfg['data']
     logger.info('Preprocessing data...')
     tf      = cfg['timeframe']
-    symbol = cfg['symbol']
-    os.makedirs(f'./src/autotrader/LeWM/data/processed/{tf}', exist_ok=True)
+    symbols = cfg['symbol']
+    os.makedirs(f'./data/processed/{tf}', exist_ok=True)
 
-    try:
-        logger.info(f'Preprocessing {symbol} {tf}')
-        path = f'./src/autotrader/LeWM/data/raw/{tf}/{symbol}.csv'
-        df = pd.read_csv(path)
-        df.set_index('OpenTime', inplace = True)
-        df.sort_index(inplace = True)
+    for symbol in symbols:
+        try:
+            logger.info(f'Preprocessing {symbol} {tf}')
+            path = f'./data/raw/{tf}/{symbol}.csv'
+            df = pd.read_csv(path)
+            df.set_index('OpenTime', inplace = True)
+            df.sort_index(inplace = True)
 
-        df['Return'] = df['Close'].pct_change()
+            df['Return'] = df['Close'].pct_change()
 
-        mu = df['Close'].rolling(window = cfg['normalization_window_size']).mean()
-        std = df['Close'].rolling(window = cfg['normalization_window_size']).std()
-        for col in ['Open', 'High', 'Low', 'Close']:
-            df[col] = (df[col] - mu) / (std + 1e-8)
+            mu = df['Close'].rolling(window = cfg['normalization_window']).mean()
+            std = df['Close'].rolling(window = cfg['normalization_window']).std()
+            for col in ['Open', 'High', 'Low', 'Close']:
+                df[col] = (df[col] - mu) / (std + 1e-8)
 
-        df['Volume'] = np.log1p(df['Volume'])
-        df['Volume'] = (df['Volume'] - df['Volume'].rolling(window = cfg['normalization_window_size']).mean()) / (df['Volume'].rolling(window = cfg['normalization_window_size']).std() + 1e-8)
-        
-        df['Volatility'] = std / mu
+            df['Volume'] = np.log1p(df['Volume'])
+            df['Volume'] = (df['Volume'] - df['Volume'].rolling(window = cfg['normalization_window']).mean()) / (df['Volume'].rolling(window = cfg['normalization_window_size']).std() + 1e-8)
+            
+            df['Volatility'] = std / mu
 
-        df.dropna(inplace=True)
-        df = df[['Open', 'High', 'Low', 'Close', 'Volume', 'Volatility', 'Return']].copy()
+            df.dropna(inplace=True)
+            df = df[['Open', 'High', 'Low', 'Close', 'Volume', 'Volatility', 'Return']].copy()
 
-        path = f'./src/autotrader/LeWM/data/processed/{tf}/{symbol}.csv'
-        df.to_csv(path)
-    except Exception as e:
-        logger.error(f'Error at {symbol}: {e}')   
+            path = f'./data/processed/{tf}/{symbol}.csv'
+            df.to_csv(path)
+        except Exception as e:
+            logger.error(f'Error at {symbol}: {e}')   
 
     logger.info('Preprocessing complete!')    
 
@@ -163,7 +165,7 @@ def get_OHLCV(SYMBOL : str, TIMEFRAME : str, SINCE : str = '2018-01-01 00:00', T
     return df
 
 def main(cfg):
-    from autotrader.util import set_logger
+    from src.util import set_logger
     logger = set_logger(cfg)
     logger.info('Starting data pipeline')
     update_data(cfg)
@@ -171,5 +173,5 @@ def main(cfg):
 
 if __name__ == '__main__':
     from omegaconf import OmegaConf
-    cfg = OmegaConf.load('./src/autotrader/LeWM/config.yaml')
+    cfg = OmegaConf.load('./config.yaml')
     main(cfg)
--- src/util.py
+++ src/util.py
@@ -1,7 +1,29 @@
 import torch
+import logging
+import sys
 
 def symlog(x):
     return torch.sign(x) * torch.log(torch.abs(x) + 1.0)
 
 def symexp(x):
     return torch.sign(x) * (torch.exp(torch.abs(x)) - 1.0)
+
+def set_logger(cfg):
+    logger = logging.getLogger(cfg['experiment_name'])
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        
+        # Tilføj tidsstempel (asctime) foran beskeden (message)
+        # datefmt bestemmer hvordan tiden ser ud (f.eks. 14:30:05)
+        formatter = logging.Formatter(
+            fmt="[%(asctime)s] %(message)s", 
+            datefmt="%Y-%m-%d %H:%M:%S"
+        )
+        
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.propagate = False 
+    
+    return logger
