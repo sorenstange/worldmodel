783cec3afe696e6d14a07b06f89e0b48fcb72dd5
Søren Stange <s204229@student.dtu.dk>
2026-07-15 15:43:42 +0200

    Initial code for jepa and actorcritic

--- src/jepa.py
+++ src/jepa.py
@@ -3,3 +3,20 @@ import torch.nn as nn
 import lightning as L
 
 from modules import *
+
+class JEPA(L.LightningModule):
+    def __init__(self, cfg):
+        super().__init__()
+        pass
+
+    def forward(self, X):
+        pass
+
+    def training_step(self, batch, batch_idx):
+        pass
+    
+    def validation_step(self, batch, batch_idx):
+        pass
+
+    def configure_optimizers(self, cfg):
+        pass
